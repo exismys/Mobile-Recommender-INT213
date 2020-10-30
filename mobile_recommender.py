51f81a5b428c7f098bde867c0f05fa97b943addc
exismys <riteshzeronity@gmail.com>
2020-10-30 16:32:46 +0530

    added complete pull-down menus and associated functions

--- mobile_recommender.py
+++ mobile_recommender.py
@@ -23,23 +23,26 @@ class MobileRecommender:
 
 
 		window = Tk()
-		window.geometry('600x400')
-		window.title("Mobile Recommender")
+		window.geometry('600x400') # Specify the size of window in pixels
+		window.title("Mobile Recommender") # Set the title of the new window
 
 		# Create menu bar
-		menubar = Menu(window)
+		menubar = Menu(window) # Create a menubar
 		window.config(menu = menubar) # Display the menu bar
 
 		# Create a pull-down menu namely "Company" and add it to menu bar
 		company_menu = Menu(menubar, tearoff = 0)
 		menubar.add_cascade(label = "Company", menu = company_menu)
 		company_menu.add_command(label = "Samsung", command = partial(self.setCompany, "Samsung"))
-		company_menu.add_command(label = "Xiomi", command = partial(self.setCompany, "Xiomi"))
+		company_menu.add_command(label = "Google Pixel", command = partial(self.setCompany, "Google Pixel"))
 		company_menu.add_command(label = "Apple", command = partial(self.setCompany, "Apple"))
 		company_menu.add_command(label = "Asus", command = partial(self.setCompany, "Asus"))
-		company_menu.add_command(label = "Oppo", command = partial(self.setCompany, "Oppo"))
+		company_menu.add_command(label = "Xiomi", command = partial(self.setCompany, "Xiomi"))
 		company_menu.add_command(label = "Huawei", command = partial(self.setCompany, "Huawei"))
-		company_menu.add_command(label = "One Plus", command = partial(self.setCompany, "One Plus"))
+		company_menu.add_command(label = "OnePlus", command = partial(self.setCompany, "OnePlus"))
+		company_menu.add_command(label = "Sony", command = partial(self.setCompany, "Sony"))
+		company_menu.add_command(label = "Oppo", command = partial(self.setCompany, "Oppo"))
+		company_menu.add_command(label = "Vivo", command = partial(self.setCompany, "Vivo"))
 
 		# Create a pull-down menu namely "RAM" and add it to menu bar
 		ram_menu = Menu(menubar, tearoff = 0)
@@ -51,6 +54,48 @@ class MobileRecommender:
 		ram_menu.add_command(label = "6 GB", command = partial(self.setRam, 6))
 		ram_menu.add_command(label = "8 GB", command = partial(self.setRam, 8))
 
+		# Create a pull-down menu namely "Storage" and add it to menu bar
+		storage_menu = Menu(menubar, tearoff = 0)
+		menubar.add_cascade(label = "Storage", menu = storage_menu)
+		storage_menu.add_command(label = "8 GB", command = partial(self.setStorage, 8))
+		storage_menu.add_command(label = "16 GB", command = partial(self.setStorage, 16))
+		storage_menu.add_command(label = "32 GB", command = partial(self.setStorage, 32))
+		storage_menu.add_command(label = "64 GB", command = partial(self.setStorage, 64))
+		storage_menu.add_command(label = "128 GB", command = partial(self.setStorage, 128))
+		storage_menu.add_command(label = "256 GB", command = partial(self.setStorage, 256))
+
+		# Create a pull-down menu namely "Battery Capacity" and add it to menu bar
+		battery_capacity_menu = Menu(menubar, tearoff = 0)
+		menubar.add_cascade(label = "Battery Capacity", menu = battery_capacity_menu)
+		battery_capacity_menu.add_command(label = "2000 mAh", command = partial(self.setBatteryCapacity, 2000))
+		battery_capacity_menu.add_command(label = "3000 mAh", command = partial(self.setBatteryCapacity, 3000))
+		battery_capacity_menu.add_command(label = "4000 mAh", command = partial(self.setBatteryCapacity, 4000))
+		battery_capacity_menu.add_command(label = "5000 mAh", command = partial(self.setBatteryCapacity, 5000))
+		battery_capacity_menu.add_command(label = "6000 mAh", command = partial(self.setBatteryCapacity, 6000))
+		battery_capacity_menu.add_command(label = "7000 mAh", command = partial(self.setBatteryCapacity, 7000))
+		battery_capacity_menu.add_command(label = "8000 mAh", command = partial(self.setBatteryCapacity, 8000))
+
+		# Create a pull-down menu namely "Price" and add it to menu bar
+		price_menu = Menu(menubar, tearoff = 0)
+		menubar.add_cascade(label = "Price", menu = price_menu)
+		price_menu.add_command(label = "10000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "15000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "20000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "30000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "40000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "50000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "70000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "100000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "125000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "150000", command = partial(self.setPrice, 10000))
+		price_menu.add_command(label = "More", command = partial(self.setPrice, 150001))
+
+		# Create a pull-down menu namely "OS" and add it to menu bar
+		os_menu = Menu(menubar, tearoff = 0)
+		menubar.add_cascade(label = "OS", menu = os_menu)
+		os_menu.add_command(label = "Android", command = partial(self.setOS, "Android"))
+		os_menu.add_command(label = "IOS", command = partial(self.setOS, "IOS"))
+
 		# To open a new window to show the details of filtered smartphones
 		def createNewWindow():
 			self.getSpecification()
@@ -58,13 +103,17 @@ class MobileRecommender:
 			window2.title("Mobile Recommendations")
 			frame1 = Frame(window2)
 			frame1.pack()
+
+			# If no record was found in the filtered list
 			if len(self.filtered) == 0:
 				print("No record found")
-				Label(frame1, text = "No record found in the database. Try selecting a different combination.").pack()
-			else:
-				row = 1
+				no_record_found_label = Label(frame1, text = "No record found in the database. Try selecting a different combination.")
+				no_record_found_label.pack()
+
+			else: # If record was found
+				row = 1 # To track the row in the grid manager
 				colors = ["#ccccff", "#ffcc99", "#ffff99", "#ffcccc", "#ccffcc"]
-				color = 0
+				color = 0 # To assign different colors to each iteration of result
 				for i in self.filtered:
 					label1 = Label(frame1, text = "Model Name: ", font = "Times 16 bold", bg = colors[color])
 					label1.grid(row = row, column = 1, sticky = W)
@@ -106,10 +155,10 @@ class MobileRecommender:
 					labelline1.grid(row = row, column = 1)
 					labelline2.grid(row = row, column = 2)
 					row += 1
-					if color == 4:
+					if color == 4: # If color is the last color, then set it to first color
 						color = 0
 					else:
-						color += 1
+						color += 1 # Otherwise set color to next color
 
 
 		# Create a frame to show details of the filtered smarphones
@@ -126,8 +175,25 @@ class MobileRecommender:
 
 	def setRam(self, ram):
 		self.ram = ram
-		print("RAM: ", self.ram)
+		print("RAM: ", self.ram, "GB")
 
+	def setStorage(self, storage):
+		self.storage = storage
+		print("Internal Storage: ", self.storage, "GB")
+
+	def setBatteryCapacity(self, capacity):
+		self.capacity = capacity
+		print("Battery Capacity: ", self.capacity, "mAh")
+
+	def setPrice(self, price):
+		self.price = price;
+		print("Price: ", self.price)
+
+	def setOS(self, os):
+		self.os = os
+		print("OS: ", os)
+
+	# To filter data and put final filtered data into self.filtered
 	def getSpecification(self):
 		temp_filtered = []
 		temp_filtered2 = []
